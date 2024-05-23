4094ae96d3112803a17211f162a9c351a2b1f956
alitanguyen <alitanguyen511@gmail.com>
2024-05-23 10:30:54 +0700

    Initial Commit

---
+++ Chapter_06/Chapter06_Exercise1.py
@@ -0,0 +1,23 @@
+#Chapter 5: Strings
+"""
+Exercise 1: Write a while loop that starts at the last character in the string and
+works its way backwards to the ﬁrst character in the string, printing each letter on
+a separate line, except backwards.
+
+"""
+fruit = 'orange'
+index = 0
+while index < len(fruit):
+    letter = fruit[(-index - 1)]
+    print(letter)
+    index = index + 1
+
+
+
+
+
+                     
+    
+
+
+
